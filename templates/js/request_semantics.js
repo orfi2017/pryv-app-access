523fc58f9f63f66b7b8367dc996c3177068dd4a1
Vlado <vlado.mitrovic@hevs.ch>
2020-02-12 17:05:58 +0100

    Display/add semantics

--- templates/js/request_semantics.js
+++ templates/js/request_semantics.js
@@ -12,15 +12,32 @@ $(function() {
     create_access(expertUsername, expertToken, data_for_create_access);
 
     consentText = 'https://pryv.com/terms-of-use/';
-//    request_semantics = JSON.parse($("#permissionsArea").val());
+
     default_permissions = create_default_permissions_data();
-    $("#permissionsArea").val(JSON.stringify(default_permissions));
+
+    loadPermissions();
+
     $("#generate-link").click(function(){
-        permissions_data = JSON.parse($("#permissionsArea").val());
+        permissions_data = default_permissions;
         campaign_name = $("#requestingCampName").val();
         app_id = 'pra-'+campaign_name;
         event_data = create_event_data(permissions_data, consentText, app_id);
         create_event(expertUsername, expertToken, event_data);
+    });
+
+    $("#addSemantic").click(function(){
+        var newSamantic = {
+            concept : {
+                type: "",
+                value: $("#concept").val()
+            },
+            level : $("#level").val(),
+            defaultName : $("#defaultName").val()
+        }
+        default_permissions.push(newSamantic);
+        $("#concept").val('');
+        $("#defaultName").val('');
+        loadPermissions()
     });
 });
 
@@ -180,3 +197,24 @@ function create_event(username, token, data){
             }
     });
 }
+
+function loadPermissions() {
+    $("#requestedPermissions").empty();
+
+    $("#requestedPermissions").append("<tr>\n" + "<th>Concept</th>\n" + "<th>Level</th>\n" + "<th>Default name</th>\n" + "<th></th>\n" + " </tr>");
+
+    default_permissions.forEach(function (element, i) {
+
+        let permission = "<tr><td>"+element.concept.value+"</td><td>"+element.level+"</td><td>"+element.defaultName+ "</td><td><i class='fa fa-trash'"+'onClick=\'deletePermission('+i+')\''+"></i></td></tr>"
+        $("#requestedPermissions").append(permission);
+    });
+}
+
+function deletePermission(index) {
+    default_permissions.splice(index, 1);
+    loadPermissions();
+}
+
+
+
+
